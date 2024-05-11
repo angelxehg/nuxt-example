4ea2992df95ed4244bd77e971d83a01b6129ba95
Angel Hurtado <angelxehg@proton.me>
2024-05-11 17:21:45 -0600

    feat: Simple page navigation

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Contact</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">/</NuxtLink></li>
+        <li><NuxtLink to="/about">/about</NuxtLink></li>
+        <li><NuxtLink to="/contact">/contact</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>My Nuxt page</h1>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">/</NuxtLink></li>
+        <li><NuxtLink to="/about">/about</NuxtLink></li>
+        <li><NuxtLink to="/contact">/contact</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
